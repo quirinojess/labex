da4865bd112e03077195d1398eab5d2b1b3af76d
quirinojess <qrnojess@gmail.com>
2022-08-03 17:03:32 -0300

    fix: change routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,41 +1,41 @@
 import React from 'react'
-import HomePage from '../pages/homePage';
+import HomePage from '../pages/homePage'
 import AdminHomePage from '../pages/adminHomePage'
 import ErrorPage from '../pages/errorPage.js'
 import LoginPage from '../pages/loginPage'
 import TripDetail from '../pages/tripDetailPage'
 import Header from '../components/header'
-import { BrowserRouter, Switch, Route, useHistory} from "react-router-dom"
+import { BrowserRouter, Switch, Route, useHistory } from 'react-router-dom'
 
-function Router(){
-return <div>
-<BrowserRouter>
-<Header/>
-<Switch>
+function Router() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/labex">
+            <HomePage />
+          </Route>
 
-<Route exact path="/labex">
-<HomePage/>
-</Route>
-    
-<Route exact path="/adminHomePage">
-<AdminHomePage/>
-</Route>
+          <Route exact path="/adminHomePage">
+            <AdminHomePage />
+          </Route>
 
-<Route exact path="/loginPage">
-<LoginPage/>
-</Route>
+          <Route exact path="/loginPage">
+            <LoginPage />
+          </Route>
 
-<Route exact path="/tripDetailPage/:id">
-<TripDetail/>
-</Route>
+          <Route exact path="/tripDetailPage/:id">
+            <TripDetail />
+          </Route>
 
-<Route>
-<ErrorPage/>
-</Route>
-</Switch>
-</BrowserRouter>
-
-</div>
+          <Route>
+            <HomePage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default Router
